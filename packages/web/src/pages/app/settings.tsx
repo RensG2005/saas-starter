9a4e330a6f15880981d6b8fe2dcd00b85f96b138
Rens Gerritsen <65126304+RensG2005@users.noreply.github.com>
2021-08-31 13:40:59 +0200

    added dashboard layout to the settings page for navigation

--- packages/web/src/pages/app/settings.tsx
+++ packages/web/src/pages/app/settings.tsx
@@ -1,5 +1,6 @@
 import { IconButton, useColorMode, Box } from '@chakra-ui/react'
 import { BiMoon, BiSun } from "react-icons/bi"
+import { DashboardLayout } from "components/DashboardLayout"
 
 export default function Settings() {
 
@@ -7,12 +8,15 @@ export default function Settings() {
   const isDark = colorMode === "dark"
 
  return (
-    <IconButton
-        borderRadius="full"
-        aria-label={`Switch to ${isDark ? "light" : "dark"} mode`}
-        variant="ghost"
-        onClick={toggleColorMode}
-        icon={<Box as={isDark ? BiSun : BiMoon} boxSize="20px" />}
-    />
+     <DashboardLayout>
+         <IconButton
+             borderRadius="full"
+             aria-label={`Switch to ${isDark ? "light" : "dark"} mode`}
+             variant="ghost"
+             onClick={toggleColorMode}
+             icon={<Box as={isDark ? BiSun : BiMoon} boxSize="20px" />}
+         />
+
+     </DashboardLayout>
  )
 }
